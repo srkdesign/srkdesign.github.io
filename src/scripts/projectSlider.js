d22fa7593bdc5257dd1f6770c7854b1e00b510a8
programmingdesigner <99723068+srkdesign@users.noreply.github.com>
2024-08-03 22:12:03 +0400

    Reverted back to most recent stable version

--- src/scripts/projectSlider.js
+++ src/scripts/projectSlider.js
@@ -5,27 +5,27 @@ import "swiper/css/bundle";
 
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
-  slidesPerView: 4,
+  slidesPerView: 2,
   spaceBetween: 20,
-  // autoplay: {
-  //   delay: 12000,
-  //   pauseOnMouseEnter: true,
-  //   disableOnInteraction: false,
-  // },
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
-  // breakpoints: {
-  //   640: {
-  //     slidesPerView: 3,
-  //   },
-  //   1024: {
-  //     slidesPerView: 4,
-  //   },
-  //   1280: {
-  //     slidesPerView: 5,
-  //   },
-  // },
-  // modules: [Autoplay, Pagination],
+  autoplay: {
+    delay: 12000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1280: {
+      slidesPerView: 5,
+    },
+  },
+  modules: [Autoplay, Pagination],
 });

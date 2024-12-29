40e5687d7a28e1c6ad73c4b66100a7b218df98fb
programmingdesigner <99723068+srkdesign@users.noreply.github.com>
2024-12-29 18:12:50 +0400

    Added playwright for safari testing and a new blog post about it

--- src/components/MarkdownComponent.jsx
+++ src/components/MarkdownComponent.jsx
@@ -4,7 +4,7 @@ const MarkdownComponent = ({ children, isFullWidth }) => {
       className={`w-full flex justify-center ${isFullWidth ? "" : "px-8"}`}
     >
       <section
-        className={`prose prose-invert prose-xl prose-headings:font-medium prose-strong:font-medium ${
+        className={`prose prose-invert prose-xl prose-headings:font-medium prose-strong:font-medium prose-blockquote:not-italic ${
           isFullWidth ? "max-w-none prose-img:mb-0 prose-img:mt-0" : ""
         }`}
       >

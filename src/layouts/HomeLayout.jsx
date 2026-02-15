255539a4a99885711900399e14e732f5f962e3bd
srkdesign <99723068+srkdesign@users.noreply.github.com>
2026-02-15 20:59:53 +0400

    Restored video blend mode

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -28,7 +28,7 @@ const HomeLayout = ({ projects, locale }) => {
           />
         </div>
 
-        <div className="md:mix-blend-lighten order-3 md:order-2">
+        <div className="mix-blend-lighten order-3 md:order-2">
           <AutoPlayVideo src="/videos/ascii_mobile.mp4" threshold={0.3} />
         </div>
 

e9de9e1575fc84ccd870065014dda46f50736660
srkdesign <99723068+srkdesign@users.noreply.github.com>
2026-02-15 20:52:33 +0400

    Downscaled video to allow playback on mobile

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -29,7 +29,7 @@ const HomeLayout = ({ projects, locale }) => {
         </div>
 
         <div className="md:mix-blend-lighten order-3 md:order-2">
-          <AutoPlayVideo src="/videos/ascii_converted.mp4" threshold={0.3} />
+          <AutoPlayVideo src="/videos/ascii_mobile.mp4" threshold={0.3} />
         </div>
 
         <div className="order-2 md:order-3 md:py-44 flex flex-col justify-end items-end">
